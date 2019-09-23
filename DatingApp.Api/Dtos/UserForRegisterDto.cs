98c550a80ca33447e319eefb516249dda210caa5
Wyatt Fry <wyattfry@gmail.com>
2019-09-23 08:52:20 -0400

    user validation

--- DatingApp.Api/Dtos/UserForRegisterDto.cs
+++ DatingApp.Api/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.Api.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Passwords must be at most 8, at least 4 characters.")]
         public string Password { get; set; }
     }
 }
